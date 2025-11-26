125ce150b939b741aed8c90cee30d33a5e4734f2
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-11-26 17:45:42 +0530

    refactor: update worker pool tests to use configuration struct for worker count and buffer size

--- pool/pool_test.go
+++ pool/pool_test.go
@@ -406,12 +406,12 @@ func TestWorkerPool_WithOptions(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			pool := NewWorkerPool[int, int](tt.opts...)
 
-			if tt.wantWorkers > 0 && pool.workerCount != tt.wantWorkers {
-				t.Errorf("expected %d workers, got %d", tt.wantWorkers, pool.workerCount)
+			if tt.wantWorkers > 0 && pool.conf.workerCount != tt.wantWorkers {
+				t.Errorf("expected %d workers, got %d", tt.wantWorkers, pool.conf.workerCount)
 			}
 
-			if tt.wantBuffer > 0 && pool.taskBuffer != tt.wantBuffer {
-				t.Errorf("expected buffer size %d, got %d", tt.wantBuffer, pool.taskBuffer)
+			if tt.wantBuffer > 0 && pool.conf.taskBuffer != tt.wantBuffer {
+				t.Errorf("expected buffer size %d, got %d", tt.wantBuffer, pool.conf.taskBuffer)
 			}
 		})
 	}
