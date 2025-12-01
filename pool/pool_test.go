fd13a3b6ad6b972c60b84ee7bf4ad220d9e57cda
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-12-01 15:12:29 +0530

    refactor: move scheduling strategy helpers to a new test_helpers.go file for better organization

--- pool/pool_test.go
+++ pool/pool_test.go
@@ -10,65 +10,6 @@ import (
 	"time"
 )
 
-// strategyConfig defines a test configuration for a scheduling strategy
-type strategyConfig struct {
-	name string
-	opts []WorkerPoolOption
-}
-
-// getAllStrategies returns all scheduling strategies to test
-// Each strategy is configured with appropriate options
-func getAllStrategies(workerCount int) []strategyConfig {
-	return []strategyConfig{
-		{
-			name: "Channel",
-			opts: []WorkerPoolOption{
-				WithWorkerCount(workerCount),
-				WithSchedulingStrategy(SchedulingChannel),
-			},
-		},
-		{
-			name: "WorkStealing",
-			opts: []WorkerPoolOption{
-				WithWorkerCount(workerCount),
-				WithSchedulingStrategy(SchedulingWorkStealing),
-			},
-		},
-		{
-			name: "MPMC",
-			opts: []WorkerPoolOption{
-				WithWorkerCount(workerCount),
-				WithMPMCQueue(WithBoundedQueue(1000)), // bounded with reasonable capacity
-			},
-		},
-		{
-			name: "PriorityQueue",
-			opts: []WorkerPoolOption{
-				WithWorkerCount(workerCount),
-				WithPriorityQueue(func(a, b int) bool {
-					return a < b
-				}),
-			},
-		},
-		{
-			name: "Bitmask",
-			opts: []WorkerPoolOption{
-				WithWorkerCount(workerCount),
-				WithSchedulingStrategy(SchedulingBitmask),
-			},
-		},
-	}
-}
-
-// getAllStrategiesWithOpts returns all scheduling strategies with additional options
-func getAllStrategiesWithOpts(workerCount int, additionalOpts ...WorkerPoolOption) []strategyConfig {
-	baseStrategies := getAllStrategies(workerCount)
-	for i := range baseStrategies {
-		baseStrategies[i].opts = append(baseStrategies[i].opts, additionalOpts...)
-	}
-	return baseStrategies
-}
-
 func TestWorkerPool_Process_BasicFunctionality(t *testing.T) {
 	strategies := getAllStrategies(4)
 
